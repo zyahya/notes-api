48e206083c225b387edfddefe0bbf636e2050068
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-06-16 15:04:12 +0300

    Remove unnecessary comments

--- Services/IJwtProvider.cs
+++ Services/IJwtProvider.cs
@@ -2,6 +2,5 @@
 
 public interface IJwtProvider
 {
-    // TODO: Try to implement this method using record instead of tuple
     (string Token, int ExpiresIn) GenerateToken(ApplicationUser user);
 }
