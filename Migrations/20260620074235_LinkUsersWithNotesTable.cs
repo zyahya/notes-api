3f29481c39d881dcafa5ee19f99c8b0c07318b9f
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-06-20 13:51:49 +0300

    Add user logic

---
+++ Migrations/20260620074235_LinkUsersWithNotesTable.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Notes.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class LinkUsersWithNotesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Notes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notes_UserId",
+                table: "Notes",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notes_AspNetUsers_UserId",
+                table: "Notes",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notes_AspNetUsers_UserId",
+                table: "Notes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Notes_UserId",
+                table: "Notes");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Notes");
+        }
+    }
+}
